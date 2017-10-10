5ae3fcc6a40c3a8914043f826ec64b7d6b40c4d2
oliver.aemmer <oliver.aemmer@NBSTD366.ch.corp.int>
2017-10-10 16:00:39 +0200

    test

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author oliver.aemmer
+ */
+
+class Student {
+    private String firstName;
+    private String lastName;
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    // Your code goes here
+   
+}
+
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void foo(String[] args) {
+
+    }
+    
+}
